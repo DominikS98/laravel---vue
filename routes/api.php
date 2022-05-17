3bdbcc83054eeec8c5942ca499ae2e74c5b567ff
Dominik Szojda <szojda.98@gmail.com>
2022-05-17 19:56:44 +0200

    make it better

--- routes/api.php
+++ routes/api.php
@@ -23,13 +23,15 @@ Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::get('/items', [ItemController::class, 'index']);
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/items/search/{name}', [ItemController::class, 'search']);
-    Route::get('/items', [ItemController::class, 'index']);
+  
     Route::prefix('/item')->group(function () {
         Route::post('/store', [ItemController::class, 'store']);
         Route::put('/{id}', [ItemController::class, 'update']);
